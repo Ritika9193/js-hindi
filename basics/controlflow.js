4bc1806bfb23376c8b9f600ad8fd5f4de478655f
Ritika9193 <118365693+Ritika9193@users.noreply.github.com>
2023-10-22 06:47:45 +0000

    done

---
+++ basics/controlflow.js
@@ -0,0 +1,45 @@
+// const num=25;
+// if(num<10){
+//     console.log("ok");
+// }
+// else if(num<75){
+//     console.log("okok");
+// }
+// else{
+//     console.log("okokok");
+// }
+// const month =3;
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2  :
+//         console.log("feb");
+//         break;
+//     case 3  :
+//         console.log("march");
+//         break;
+
+//     default:
+//         console.log("default");
+//         break;
+// }
+/*falsey values
+false,0,-0, bigInt 0n,"",null undefined,NaN
+
+Truthy values=>
+"0","false"," ",[],{},function(){} //empty function
+*/
+/*to check if object is empty
+const user={};
+if(Object.keys(user).length===0){
+    console.log("empty");
+}*/
+//Nullish Coalescing Operator(??): Null Undefined
+let val1;
+//val1=null??10;
+//val2=undefined??10;
+
+/* Ternary Operator
+condition?true:false
+*/
