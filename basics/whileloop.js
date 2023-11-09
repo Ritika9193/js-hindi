f533a31d2493268799b95867933123245dcda891
Ritika9193 <118365693+Ritika9193@users.noreply.github.com>
2023-11-09 06:12:51 +0000

    done

---
+++ basics/whileloop.js
@@ -0,0 +1,11 @@
+let myarr=["abc","def","ghi"];
+let arr=0;
+while(arr<myarr.length){
+    // console.log(`value is ${myarr[arr]}`);
+    arr+=1;
+}
+let i=11
+do {
+    console.log(`${i}`);
+    i++;
+} while (i<=10);
