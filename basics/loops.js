6ce4cdc78f93756954e36830a56562b256654822
Ritika9193 <118365693+Ritika9193@users.noreply.github.com>
2023-11-13 13:45:53 +0000

    change

--- basics/loops.js
+++ basics/loops.js
@@ -2,5 +2,18 @@
 
 const arr=[1,2,3,4,5];
 for (const i of arr) {
-    console.log(i);
+    // console.log(i);
+}
+const greetings='hello world';
+for (const greet of greetings) {
+    // console.log(`letter is ${greet}`);
+}
+const map=new Map()
+map.set("1","a")
+map.set("2","b")
+map.set("3","c")
+//console.log(map);
+
+for (const [i,j] of map) {
+    console.log(i,":-",j);
 }
