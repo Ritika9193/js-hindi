8807924bdb6dbdf139ff3a505a2edc9afdd887d0
Ritika9193 <118365693+Ritika9193@users.noreply.github.com>
2023-11-22 13:05:32 +0000

    done

--- iterators/six.js
+++ iterators/six.js
@@ -1,7 +1,36 @@
 const nums=[1,2,3,4,5]
 const newnum=nums.filter( (nums) => { 
-    return nums>2 //agar curly braces use kia h then return kewword use krna imp h beacuase of scope
+    return nums>2 //agar curly braces use kia h then return keyword use krna imp h beacuase of scope
 })
-console.log(newnum);
+// console.log(newnum);
 const newsnum=nums.filter( (nums) => nums>2) //no need of return keyword
-console.log(newsnum);
+// console.log(newsnum);
+const coding=[
+    {
+        no:'1',
+        letter:'a'
+    },
+    {
+        no:'2',
+        letter:'a'
+    },
+    {
+        no:'3',
+        letter:'c'
+    }
+]
+let newcoding=coding.filter(( i)=> i.letter==='a');
+newcoding=coding.filter((i)=>{ 
+    return i.no>=2 && i.letter==='a'
+})
+//console.log(newcoding);
+ 
+const arr= [ 1,2,3,4,5 ]
+const newarr=arr.map((a)=>{return a+10})
+//console.log(newarr);
+
+//CHAINING
+const newarrs=arr.map((num)=> {return num+10})
+                 .map((num)=>{return num+1})
+                .filter((num)=>{return num>=13})
+console.log(newarrs)
