f0572108e94319848d0843cd9e29030df9692e33
Ritika9193 <118365693+Ritika9193@users.noreply.github.com>
2023-12-09 13:26:59 +0000

    done

--- 10_classes/myclass.js
+++ 10_classes/myclass.js
@@ -1,13 +1,33 @@
-class user{
-    constructor(username,email,password){
-        this.username=username;
-        this.email=email;
-        this .password=password;
-    }
+// class user{
+//     constructor(username,email,password){
+//         this.username=username;
+//         this.email=email;
+//         this .password=password;
+//     }
 
-    encryptpassword(){
-        console.log(`${this.password}abc`);
-    }
+//     encryptpassword(){
+//         console.log(`${this.password}abc`);
+//     }
+//     changeusername(){
+//         console.log((`${this.username.toUpperCase()}`));
+//     }
+// }
+// const chai=new user("chai","chai@gmail.com",123)
+// console.log(chai.encryptpassword());
+// console.log(chai.changeusername());
+
+//behind the scene
+function user(username,email,password){
+    this.username=username;
+    this.email=email;
+    this.password=password;
+}
+user.prototype.encryptpassword=function(){
+    console.log(`${this.password}abc`);
+}
+user.prototype.changeusername=function(){
+    console.log((`${this.username.toUpperCase()}`));
 }
 const chai=new user("chai","chai@gmail.com",123)
 console.log(chai.encryptpassword());
+console.log(chai.changeusername());
